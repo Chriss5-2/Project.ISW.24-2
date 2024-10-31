1622ec3203121c2e512c56f5cb3bf84b877822ea
Chrizzz <christiangiovannixd@gmail.com>
2024-10-31 15:59:05 -0500

    Clase carrito

---
+++ src/Aplicacion/Entity/Carrito.java
@@ -0,0 +1,20 @@
+package Aplicacion.Entity;
+
+import java.util.List;
+
+public class Carrito {
+    public void addProductToCar(Producto producto){
+    }
+    public void deleteProductToCar(Producto producto){
+    }
+    public List<Producto> showCar(){
+        return List.of();
+    }
+    public void confirmar(){
+
+    }
+    public Pedido generarPedido(){
+        return null;
+    }
+
+}
