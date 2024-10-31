209937433533868251c519220d3caa5513b83b20
Chrizzz <christiangiovannixd@gmail.com>
2024-10-31 11:12:39 -0500

    Clase Persona avanzada

---
+++ src/CompPago/ComprobantePago.java
@@ -0,0 +1,39 @@
+package CompPago;
+
+import java.security.Timestamp;
+
+public class ComprobantePago {
+    private Timestamp fecha;
+    private double monto;
+    private int nroComprobante;
+
+    public ComprobantePago(Timestamp fecha, double monto, int nroComprobante) {
+        this.fecha = fecha;
+        this.monto = monto;
+        this.nroComprobante = nroComprobante;
+    }
+
+    public Timestamp getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Timestamp fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public int getNroComprobante() {
+        return nroComprobante;
+    }
+
+    public void setNroComprobante(int nroComprobante) {
+        this.nroComprobante = nroComprobante;
+    }
+}
