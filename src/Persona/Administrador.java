43b573d154f3a782fe7bd8b1c1ce9b3643c2389e
Chrizzz <christiangiovannixd@gmail.com>
2024-10-31 11:55:23 -0500

    Clase Administrador que hereda de Empleado

---
+++ src/Persona/Administrador.java
@@ -0,0 +1,10 @@
+package Persona;
+
+import java.util.Date;
+
+public class Administrador extends Empleado{
+
+    public Administrador(int idPersona, String nombre, String apellidoPaterno, String apellidoMaterno, String direccion, String telefono, String email, Date fechaNacimiento, int idEmpleado, double salario, Date fechaContratacion, String horario, String password) {
+        super(idPersona, nombre, apellidoPaterno, apellidoMaterno, direccion, telefono, email, fechaNacimiento, idEmpleado, salario, fechaContratacion, horario, password);
+    }
+}
